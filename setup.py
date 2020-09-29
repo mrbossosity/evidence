a8e3935711ac0c61a741bebf334f2212d039ec41
mrbossosity <mrbossosity@gmail.com>
2020-09-29 07:25:49 -1000

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "rb") as f:
+    long_descr = f.read()
+
+setup(
+    name="evidence with ease",
+    version="1.0",
+    packages=["evidence"],
+    description="A nifty Python tool for expediting evidence printing.",
+    long_description=long_descr,
+    long_description_content_type="text/markdown", 
+    entry_points = {
+        "console-scripts":["evidence = evidence-with-ease.evidence:main"]
+    },
+    author="mrbossosity",
+    author_email="mrbossosity@gmail.com"
+)
