e2fd3e4051dffd27f745bd073cef235cbb816ce3
mrbossosity <mrbossosity@gmail.com>
2020-10-01 08:08:22 -1000

    headers and downloadFolder initialized *before* job

--- evidence/__main__.py
+++ evidence/__main__.py
@@ -8,18 +8,16 @@ from bs4 import BeautifulSoup
 from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 
 # important global variables:
+downloadFolder = False
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "Accept-Encoding": "gzip",
+    "Accept-Language": "en-US,en;q=0.9,es;q=0.8",
+    "Upgrade-Insecure-Requests": "1",
+    "Referer": 'https://google.com',
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
+}
 def initVars():
-    global downloadFolder
-    downloadFolder = False
-    global headers
-    headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        "Accept-Encoding": "gzip",
-        "Accept-Language": "en-US,en;q=0.9,es;q=0.8",
-        "Upgrade-Insecure-Requests": "1",
-        "Referer": 'https://google.com',
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
-    }
     global file_paths
     file_paths = []
     global failed_files
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="evidence with ease",
-    version="1.1.2",
+    version="1.1.3",
     packages=["evidence"],
     description="A nifty Python tool for expediting evidence printing.",
     long_description=long_descr,
